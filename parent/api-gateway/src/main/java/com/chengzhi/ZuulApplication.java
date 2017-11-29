ebfc982256c5d89ded0360e0880ca3b377987a7d
chengzhi <ruanchengzhia@outlook.com>
2017-11-29 17:43:29 +0800

    gateway

---
+++ parent/api-gateway/src/main/java/com/chengzhi/ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.chengzhi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulApplication {
+	public static void main(String[] args) {
+	}
+}
