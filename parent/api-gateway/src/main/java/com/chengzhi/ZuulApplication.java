daf35ef196c7887a2932f2bc9c4bf0fbd0a4da6f
chengzhi <ruanchengzhia@outlook.com>
2017-12-05 15:05:24 +0800

    git add config

--- parent/api-gateway/src/main/java/com/chengzhi/ZuulApplication.java
+++ parent/api-gateway/src/main/java/com/chengzhi/ZuulApplication.java
@@ -2,8 +2,11 @@ package com.chengzhi;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,6 +15,14 @@ public class ZuulApplication {
 	
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+	}
+	/**
+	 * 动态路由
+	 */
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties() {
+		return new ZuulProperties();
 	}
 	/*@Bean
 	public AccessFilter accessFilter() {
